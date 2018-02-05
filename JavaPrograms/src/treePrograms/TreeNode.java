e09c29cf53b9773a34924aa672803ad5ed3dc832
sourabh.singh <sourabh.singh@franconnect.net>
2018-02-05 12:15:25 +0530

    First Commit

---
+++ JavaPrograms/src/treePrograms/TreeNode.java
@@ -0,0 +1,22 @@
+package treePrograms;
+
+public class TreeNode {
+	public int data;
+	TreeNode left , right;
+	TreeNode(int key){
+		data = key;
+		left = right = null;
+	}
+	
+	public static TreeNode getTree(){
+		TreeNode node = new TreeNode(1);
+		node.left = new TreeNode(2);
+		node.right = new TreeNode(3);
+		
+		node.left.left = new TreeNode(4);
+		node.left.right = new TreeNode(5);
+		node.right.left = new TreeNode(6);
+		node.right.left = new TreeNode(7);
+		return node;
+	}
+}
